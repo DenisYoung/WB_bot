322f8eb4f0ced48c266e731641af5910f3860a9e
Denis <math2019@yandex.ru>
2024-03-16 17:49:59 +0300

    Request update

--- bot/wb_request.py
+++ bot/wb_request.py
@@ -10,18 +10,32 @@ def qty_calc(sizes):
 
 
 def get_info_from_wb(article):
-    r = requests.get(url=f"https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=30&nm={article}").json()
-    if len(r["data"]["products"]) == 0:
-        return 0
-    data = {
-        "name": r["data"]["products"][0]["name"], 
-        "article": article, 
-        "price": r["data"]["products"][0]["salePriceU"], 
-        "product_rating": r["data"]["products"][0]["reviewRating"], 
-        "quantity_of_product": qty_calc(r["data"]["products"][0]["sizes"])
-
+    url = f"https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=30&nm={article}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"HTTP Request failed: {e}")
+        return None
+    
+    try:
+        data = response.json()
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding failed: {e}")
+        return None
+    
+    if not data.get("data", {}).get("products"):
+        return None
+    
+    product = data["data"]["products"][0]
+    product_info = {
+        "name": product["name"],
+        "article": article,
+        "price": product["salePriceU"],
+        "product_rating": product["reviewRating"],
+        "quantity_of_product": qty_calc(product["sizes"])
     }
-    return data
+    return product_info
 
 
 #print(get_info_from_wb(200))
